7ba84b5c5dfdfa12a7beff4a4d2299b983645603
AndrejsAbrickis <andrejs.abrickis@mintos.com>
2020-09-13 15:33:49 +0300

    fix: Resolves svg module import error

--- PlanningPoker.Web/vue.config.js
+++ PlanningPoker.Web/vue.config.js
@@ -2,4 +2,16 @@ module.exports = {
   outputDir: 'wwwroot/public',
   filenameHashing: false,
   lintOnSave: false,
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
 };
